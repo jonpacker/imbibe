319ed3ac7bba837eafcd19f8b8a07e8b24124775
Jon Packer <contact@jonpacker.com>
2013-11-19 15:41:48 +0100

    test harness

---
+++ test/test.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var imbibe = require('../');
+
+describe('imbibe', function() {
+  var serverRoot;
+  var server;
+  before(function(done) {
+    server = http.createServer(function(req, res) {
+      if (req.url == '/first') {
+        res.writeHead(200);
+        res.end(JSON.stringify({ value: 'first' }));  
+      } else if (req.url == '/second') {
+        res.writeHead(200);
+        res.end(JSON.stringify({ value: 'second' }));
+      } else if (req.url == '/bad') {
+        res.writeHead(400);
+        res.end();
+      } else if (req.url == '/broken') {
+        res.writeHead(200);
+        res.end("{ value: 'broken }");
+      } else {
+        res.writeHead(404);
+        res.end();
+      }
+    });
+    server.listen(0, function(err) {
+      if (err) return done(err);
+      serverRoot = 'http://localhost:' + server.address().port;
+    });
+  });
+  after(function(done) {
+    server.close(done);
+  });
+});
