a2e7118d5944cb0e444227efabad179ed31821bf
Jon Packer <contact@jonpacker.com>
2013-11-19 16:39:00 +0100

    implement error handling

--- imbibe.js
+++ imbibe.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var codes = require('http').STATUS_CODES;
 
 module.exports = function(url, opts, callback) {
   if (typeof opts == 'function') {
@@ -11,8 +12,12 @@ module.exports = function(url, opts, callback) {
 
   return request(opts, function(err, response, body) {
     if (err) callback(err);
-    else if (response.statusCode >= 300) 
-      callback(new Error(body || response.status || response.statusCode));
-    else callback(null, body);
+    else if (response.statusCode >= 300) {
+      if (body) return callback(new Error(body));
+      var error = new Error(codes[response.statusCode]);
+      error.statusCode = response.statusCode;
+      error.response = response;
+      callback(error);
+    } else callback(null, body);
   });
 };
